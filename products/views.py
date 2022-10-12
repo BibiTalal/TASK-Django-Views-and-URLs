88118e849c8893b179952f45882f127720b842b8
BibiTalal <bibi.t_alfahad@hotmail.com>
2022-10-12 19:12:11 +0300

    task completed

--- products/views.py
+++ products/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Product
 
 
 def get_home(request):
     return HttpResponse("<h1> Welcome</h1>")
+
+
+def get_product(request, product_id):
+    products = Product.objects.get(id=product_id)
+    return HttpResponse(
+        f"Name: {products.name}, Price: {products.price}, Description: {products.description} "
+    )
